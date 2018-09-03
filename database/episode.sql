aa09a1855841578bf4c9bc20431a937b0bcfc126
xudusheng <597132100@qq.com>
2018-09-04 00:30:09 +0800

    no message

---
+++ database/episode.sql
@@ -0,0 +1,3 @@
+INSERT INTO episode(ekey, md5key, title, href, sort, section) VALUES ('53b711843864133eeb2d0b1b04ebe82210', '53b711843864133eeb2d0b1b04ebe822', '高清中字', '/play/38901/1/1.html', '0', '1');
+INSERT INTO episode(ekey, md5key, title, href, sort, section) VALUES ('65966d20ffbcbc78deba1b580558f4cf10', '65966d20ffbcbc78deba1b580558f4cf', '第1集', '/play/44805/1/1.html', '0', '1');
+INSERT INTO episode(ekey, md5key, title, href, sort, section) VALUES ('65966d20ffbcbc78deba1b580558f4cf11', '65966d20ffbcbc78deba1b580558f4cf', '第2集', '/play/44805/1/2.html', '1', '1');
