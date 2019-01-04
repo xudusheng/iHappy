dce3481407fe13a148662ae6029f053dd2c20939
xudusheng <597132100@qq.com>
2019-01-04 23:51:30 +0800

    no message

--- iHappy/iHappy/Setting/XDSSettingVC.m
+++ iHappy/iHappy/Setting/XDSSettingVC.m
@@ -7,6 +7,7 @@
 //
 
 #import "XDSSettingVC.h"
+#import "BXRetailStoreGuideVC.h"
 
 @interface XDSSettingVC ()
 @property (weak, nonatomic) IBOutlet UILabel *memeryLabel;
@@ -29,7 +30,11 @@
 
 #pragma mark - delegate method 代理方法
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == 0 && indexPath.row == 1) {
+    if (indexPath.section == 0 && indexPath.row == 0) {
+        BXRetailStoreGuideVC *guideVC = [[BXRetailStoreGuideVC alloc] init];
+        [self.navigationController pushViewController:guideVC animated:YES];
+
+    }else if (indexPath.section == 0 && indexPath.row == 1) {
         [self clearMemery];
     }else if (indexPath.section == 1 && indexPath.row == 0) {
         [self shareAction];
---
+++ iHappy/iHappy/guidement/BXRetailStoreGuideVC.h
@@ -0,0 +1,17 @@
+//
+//  BXRetailStoreGuideVC.h
+//  iHappy
+//
+//  Created by Hmily on 2019/1/4.
+//  Copyright © 2019 Dusheng Du. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BXRetailStoreGuideVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
